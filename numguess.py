55851d3530d7471513eec3ba5cc145863b2e160c
ThePythonDoge <utdoge@gmail.com>
2017-09-18 13:27:23 -0600

    Update numguess.py
    MUCH BETTER <3

--- numguess.py
+++ numguess.py
@@ -1,11 +1,24 @@
 from random import randint
-rand = randint(1, 100)
-print("Welcome to this tech demo. You will try to guess a random number between 1 and 100.")
-guess = int(input("Please enter an integer."))
 
-while guess != rand:
+print("Welcome to this tech demo. You will try to guess a random number between 1 and 100.\n")
+
+rand = randint(1, 100)
+
+def game():
+  try:
+    
+    guess = int(input("Please enter an integer.\n"))
+  
+  except ValueError:
+    print("Enter a number please.")
+    game() # goes back until a number is entered.
+  #print(rand) for debugging purposes
+  
+  if rand != guess: #No need for loop if you got function
     print("Unfortunately, your guess is wrong.")
-    guess = int(input("Please try again."))
-while rand == guess:
+    game()
+  
+  else: #you can do "elif rand == guess" but "else" is a more nice thing
     print("Your guess is correct! Thank you for testing this tech demo.")
-    stop = str(input("Please enter any kind of text to end this program."))
+    exit(2) #Quits program without errors
+game()    
